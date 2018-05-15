1556019108ec5428f1b0bf42be6eac4d5734c22b
Calendis <calendis1@gmail.com>
2018-05-14 19:42:05 -0600

    Started indoor tileset, added a test house.

---
+++ resources/tilesets/indoor_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="indoor_0" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../../../../../../media/removable/SD Card P0/Programming/Python/Moderately Seafaring/resources/img/tilesets/indoor_tileset_0.png" trans="ffffff" width="512" height="512"/>
+</tileset>
